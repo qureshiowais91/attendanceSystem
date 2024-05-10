a7745b40f5296f97fa9c8d3e394e5e646cc4c057
qureshiowais <qureshiowais91@gmail.com>
2024-05-10 15:00:33 +0530

    loader added in add student and error handling

--- src/pages/Dashboard/Parent/AddStudent/AddStudent.jsx
+++ src/pages/Dashboard/Parent/AddStudent/AddStudent.jsx
@@ -1,12 +1,21 @@
 import { useState, useEffect } from 'react';
-import { TextField, Button, Grid, Typography, Container } from '@mui/material';
+import {
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  Container,
+  CircularProgress,
+} from '@mui/material';
 import { addStudent, listClassroom, joinClassroom } from '../../../../API/APIs';
 import { useSelector } from 'react-redux';
 
 const StudentDetailsForm = () => {
   const token = useSelector((state) => state.auth.jwt);
-
+  const [error, setError] = useState();
   const [classrooms, setClassrooms] = useState([]);
+  const [loading, setLoading] = useState();
+
   const [student, setStudent] = useState({
     name: '',
     birthdate: '',
@@ -29,42 +38,40 @@ const StudentDetailsForm = () => {
     };
 
     try {
+      setLoading(true);
+
       // Create new Student
       const res = await addStudent(payload);
       const isDone = await res.json();
-      // console.log(isDone);
-      setTimeout(async () => {
-        (async () => {
-          if (isDone.message === 'Student added successfully') {
-            const joinPayload = {
-              studentId: isDone.newStudent._id,
-              classroomId: student.classRoom,
-            };
-            const payload = {
-              joinPayload: joinPayload,
-              token: token,
-            };
 
-            const joined = await joinClassroom(payload);
-            await joined.json();
-            // console.log(data);
-            alert('Student added successfully');
-          }
-        })();
-      }, 1000);
+      // Add Student to Classroom if student creation is successful
+      if (isDone.message === 'Student added successfully') {
+        const joinPayload = {
+          studentId: isDone.newStudent._id,
+          classroomId: student.classRoom,
+        };
+        const payload = {
+          joinPayload: joinPayload,
+          token: token,
+        };
 
-      // Add Student to Classroom
-      // console.log(student.classRoom);
+        const joined = await joinClassroom(payload);
+        await joined.json();
+        alert('Student added successfully');
+      } else {
+        throw new Error('Failed to add student');
+      }
     } catch (error) {
       alert(error);
+    } finally {
+      setLoading(false);
+      setStudent({
+        name: '',
+        birthdate: '',
+        classRoom: '',
+      });
     }
-
     // Reset form fields after submission
-    setStudent({
-      name: '',
-      birthdate: '',
-      classRoom: '',
-    });
   };
 
   useEffect(() => {
@@ -78,6 +85,10 @@ const StudentDetailsForm = () => {
         const list = await res.json();
 
         setClassrooms(list);
+        console.log(list);
+        if (list === 'schoolId is Missing') {
+          setError('You Must First Join A School');
+        }
       } catch (error) {
         console.error('Error fetching classrooms:', error);
       }
@@ -88,65 +99,71 @@ const StudentDetailsForm = () => {
 
   return (
     <Container maxWidth='sm'>
-      <Typography variant='h4' align='center' gutterBottom>
-        Add Student
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label='Name'
-              name='name'
-              value={student.name}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label='Birthdate'
-              name='birthdate'
-              type='date'
-              value={student.birthdate}
-              onChange={handleChange}
-              InputLabelProps={{ shrink: true }}
-              inputProps={{
-                max: new Date().toISOString().split('T')[0], // Set max date to today
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            {classrooms.length === 0 ? (
-              <Typography variant='body1'>
-                Did not join a school? Contact school admin to add classrooms.
-              </Typography>
-            ) : (
-              <TextField
-                fullWidth
-                select
-                label='Classroom'
-                name='classRoom'
-                value={student.classRoom}
-                onChange={handleChange}
-                SelectProps={{ native: true }}
-              >
-                <option value=''></option>
-                {classrooms?.map((classroom) => (
-                  <option key={classroom._id} value={classroom._id}>
-                    {classroom.classroom}
-                  </option>
-                ))}
-              </TextField>
-            )}
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant='contained' color='primary' type='submit'>
-              Submit
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+      {error ? (
+        <Typography variant='body1'>
+          Did not join a school? Contact school admin to add classrooms.
+        </Typography>
+      ) : (
+        <>
+          <Typography variant='h4' align='center' gutterBottom>
+            Add Student
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label='Name'
+                  name='name'
+                  value={student.name}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label='Birthdate'
+                  name='birthdate'
+                  type='date'
+                  value={student.birthdate}
+                  onChange={handleChange}
+                  InputLabelProps={{ shrink: true }}
+                  inputProps={{
+                    max: new Date().toISOString().split('T')[0], // Set max date to today
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  select
+                  label='Classroom'
+                  name='classRoom'
+                  value={student.classRoom}
+                  onChange={handleChange}
+                  SelectProps={{ native: true }}
+                >
+                  <option value=''></option>
+                  {classrooms?.map((classroom) => (
+                    <option key={classroom._id} value={classroom._id}>
+                      {classroom.classroom}
+                    </option>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={12}>
+                {loading ? (
+                  <CircularProgress size={24} />
+                ) : (
+                  <Button variant='contained' color='primary' type='submit'>
+                    Submit
+                  </Button>
+                )}
+              </Grid>
+            </Grid>
+          </form>
+        </>
+      )}
     </Container>
   );
 };
