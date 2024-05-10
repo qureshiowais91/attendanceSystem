1e51a2b21c8a0e70bfe2157fdff306ebcd35ad6d
qureshiowais <qureshiowais91@gmail.com>
2024-05-10 19:17:49 +0530

    loader and error handled basic

--- src/pages/Dashboard/Admin/CreateClassroom/CreateClassroom.jsx
+++ src/pages/Dashboard/Admin/CreateClassroom/CreateClassroom.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react';
-import { TextField, Button, Grid, Typography, Container } from '@mui/material';
+import {
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  Container,
+  Alert,
+  CircularProgress,
+} from '@mui/material';
 import { createClassroom } from '../../../../API/APIs'; // Assuming you have an API function for creating a classroom
 import { useSelector } from 'react-redux';
 
 const CreateClassroomForm = () => {
   const [classroom, setClassroom] = useState({});
+
+  const [Loading, setLoading] = useState();
+  const [error, setError] = useState();
+  const [res, setRes] = useState();
   const token = useSelector((state) => state.auth.jwt);
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,8 +28,9 @@ const CreateClassroomForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
+      setLoading(true);
+
       const payload = {
         token: token,
         classroom: classroom,
@@ -27,16 +40,22 @@ const CreateClassroomForm = () => {
       const isDone = await res.json();
 
       if (isDone.message === 'Classroom created successfully') {
-        alert('Classroom created successfully');
+        setRes('Classroom created successfully');
         setClassroom({ name: '' }); // Reset form fields after successful creation
       }
     } catch (error) {
+      setError(error);
       alert('Error creating classroom:', error);
+      setLoading(false);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <Container maxWidth='sm'>
+      {res && <Alert severity='info'>{res}</Alert>}
+      {error && <Alert severity='error'>{error}</Alert>}
       <Typography variant='h4' align='center' gutterBottom>
         Create Classroom
       </Typography>
@@ -52,9 +71,13 @@ const CreateClassroomForm = () => {
             />
           </Grid>
           <Grid item xs={12}>
-            <Button variant='contained' color='primary' type='submit'>
-              Create Classroom
-            </Button>
+            {Loading ? (
+              <CircularProgress size={24} />
+            ) : (
+              <Button variant='contained' color='primary' type='submit'>
+                Create Classroom
+              </Button>
+            )}
           </Grid>
         </Grid>
       </form>
