b726085ca5eec69a61d355926717ff275d2ae8f3
qureshiowais <qureshiowais91@gmail.com>
2024-05-29 14:35:32 +0530

    public profile

---
+++ src/pages/Dashboard/Admin/Profile/PublicProfile.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { publicprofile } from '../../../../API/APIs';
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  Box,
+} from '@mui/material';
+
+function PublicProfile() {
+  const { userId } = useParams(); // Assuming the school ID is passed as a parameter in the URL
+  const [school, setSchool] = useState(null);
+
+  useEffect(() => {
+    const fetchSchoolData = async () => {
+      try {
+        const payload = {
+          query: userId,
+        };
+        const response = await publicprofile(payload);
+        console.log(userId);
+        if (!response.ok) {
+          throw new Error('Failed to fetch school data');
+        }
+        const data = await response.json();
+        setSchool(data);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching school data:', error);
+      }
+    };
+
+    fetchSchoolData();
+  }, [userId]);
+
+  if (!school) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Box p={2} border={1} borderRadius={4}>
+      <Typography variant='h2'>{school.name}</Typography>
+
+      <Box mt={2}>
+        <Typography>
+          <strong>Address:</strong> {school.address}
+        </Typography>
+      </Box>
+
+      <Divider variant='middle' />
+
+      <Box mt={2}>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary={<strong>Phone:</strong>}
+              secondary={school.contactDetails}
+            />
+          </ListItem>
+        </List>
+      </Box>
+
+      <Divider variant='middle' />
+
+      <Box mt={2}>
+        <Typography>
+          <strong>Established Date:</strong> {school.establishedDate}
+        </Typography>
+      </Box>
+
+      <Divider variant='middle' />
+
+      <Box mt={2}>
+        <Typography>
+          <strong>Facilities:</strong> {school.facilities.join(', ')}
+        </Typography>
+      </Box>
+      {/* Add more details as needed */}
+    </Box>
+  );
+}
+
+export default PublicProfile;
