58e7c6d2d308916d0099d77c573bafae766f7045
qureshiowais <qureshiowais91@gmail.com>
2024-06-15 16:17:50 +0530

    schoolID

--- src/pages/Dashboard/Teacher/Scanner/StaffAttandaceScanner.jsx
+++ src/pages/Dashboard/Teacher/Scanner/StaffAttandaceScanner.jsx
@@ -2,20 +2,18 @@ import QRScanner from '../../../../components/qr_scanner/QRScanner';
 import { useState } from 'react';
 import { staffAttendance } from '../../../../API/APIs';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
 export const StaffAttandaceScanner = () => {
   const [scannedData, setScannedData] = useState('');
   const token = useSelector((state) => state.auth.jwt);
-  const navigate = useNavigate();
 
   // Define the onScan callback function
-  const handleScan = (data) => {
-    setScannedData(data);
+  const handleScan = (schoolID) => {
+    setScannedData(schoolID);
     (async () => {
       const payload = {
         token,
-        schoolId: data,
+        schoolId: schoolID || "" ,
         approximateLocation: 'will add later',
       };
       await staffAttendance(payload);
